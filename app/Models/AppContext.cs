af4bd8f4a0c877c2373eb1766ef976873003898a
Juk3n <adekenn3@gmail.com>
2024-07-30 11:36:19 +0200

    Added models with context

---
+++ app/Models/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.Models;
+
+public class AppContext : DbContext
+{
+    public DbSet<Robot> Robots { get; set; }
+    public DbSet<JobStatus> JobStatuses { get; set; }
+    public DbSet<LoadInfo> LoadInfos { get; set; }
+    public DbSet<Localization> Localizations { get; set; }
+    public DbSet<Position> Positions { get; set; }
+    public DbSet<Status> Statuses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=RobotsDb;Trusted_Connection=True;TrustServerCertificate=true;");
+    }
+}
---
+++ app/Models/Robot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.Models;
+
+public class Robot
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Battery { get; set; }
+    public required Localization Localization { get; set; }
+    public required Position Position { get; set; }
+
+    public required Status Status { get; set; }
+    public required LoadInfo LoadInfo { get; set; }
+    public required JobStatus JobStatus { get; set; }
+
+}
